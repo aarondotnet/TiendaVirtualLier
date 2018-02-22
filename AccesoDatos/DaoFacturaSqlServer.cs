ac632d871272e2474edd624044a154bfdc93dd06
Aaron <2d9ph7@svk.jp>
2018-02-22 17:44:16 +0100

    Instert LineasFactura

--- AccesoDatos/DaoFacturaSqlServer.cs
+++ AccesoDatos/DaoFacturaSqlServer.cs
@@ -11,7 +11,7 @@ namespace TiendaVirtual.AccesoDatos
     class DaoFacturaSqlServer : IDaoFactura
     {
         private const string SQL_INSERT = "INSERT INTO facturas (Numero, Fecha,UsuariosId) VALUES ((select count(*) from facturas),@fecha,@UsuariosId)";
-        private const string SQL_INSERT2 = "INSERT INTO lineasfactura (FacturaId,ProductoId,Cantidad) VALUES (@FacturaID, @ProductoId, @Cantidad)";
+        private const string SQL_INSERT2 = "INSERT INTO lineasfactura (FacturaId,ProductoId,Cantidad) VALUES ((select MAX(ID) from facturas), @ProductoId, @Cantidad)";
 
         private string connectionString;
         public DaoFacturaSqlServer(string connectionString)
@@ -67,7 +67,56 @@ namespace TiendaVirtual.AccesoDatos
             {
                 throw new AccesoDatosException("No se ha podido realizar el alta", e);
             }
-            
+            foreach (ILineaFactura linea in factura.LineasFactura)
+            {
+                try
+                {
+                    using (IDbConnection con = new System.Data.SqlClient.SqlConnection(connectionString))
+                    {
+                        //"Zona declarativa"
+                        con.Open();
+
+                        IDbCommand comInsert = con.CreateCommand();
+
+                        comInsert.CommandText = SQL_INSERT2;
+
+                        //IDbDataParameter parFacturaID = comInsert.CreateParameter();
+                        //parFacturaID.ParameterName = "FacturaID";
+                        //parFacturaID.DbType = DbType.String;
+
+                        IDbDataParameter parProductoId = comInsert.CreateParameter();
+                        parProductoId.ParameterName = "ProductoId";
+                        parProductoId.DbType = DbType.String;
+
+                        IDbDataParameter parCantidad = comInsert.CreateParameter();
+                        parCantidad.ParameterName = "Cantidad";
+                        parCantidad.DbType = DbType.String;
+
+                        //comInsert.Parameters.Add(parFacturaID);
+                        comInsert.Parameters.Add(parProductoId);
+                        comInsert.Parameters.Add(parCantidad);
+
+                        //"Zona concreta"
+                        //parFacturaID.Value = factura.Numero;
+                        parProductoId.Value = linea.Producto.Id;
+                        parCantidad.Value = factura.Usuario.Id;
+
+                        int numRegistrosInsertados = comInsert.ExecuteNonQuery();
+
+                        if (numRegistrosInsertados != 1)
+                            throw new AccesoDatosException("Número de registros insertados: " +
+                                numRegistrosInsertados);
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new AccesoDatosException("No se ha podido realizar el alta", e);
+                }
+
+            }
+
+              
+
         }
     }
 }
